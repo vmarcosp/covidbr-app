0d28f56ce1bced688c64a3ffc16448773a1bfd88
Marcos <vmarcosp.pereira@gmail.com>
2020-03-23 20:26:33 -0300

    chore: Adding favicon and logo

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { App } from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 )
 
-serviceWorker.unregister()
+serviceWorker.register()
