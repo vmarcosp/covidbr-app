c24be87813020412c01058feb17b7b3c094e41f0
Marcos <vmarcosp.pereira@gmail.com>
2020-03-24 20:54:48 -0300

    chore: Removing unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+
+import { GlobalStyles } from 'css/GlobalStyles'
 import { Home } from './pages/home'
 
 import 'leaflet/dist/leaflet.css'
-import { GlobalStyles } from 'css/GlobalStyle'
 
 const client = new ApolloClient({
   uri: process.env.API_URL
-});
+})
 
 export const App = () => (
   <ApolloProvider client={client}>
