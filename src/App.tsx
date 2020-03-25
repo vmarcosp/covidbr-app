371feeef00d8db28ab64efd29b70760431744b7e
Marcos <vmarcosp.pereira@gmail.com>
2020-03-25 18:58:00 -0300

    fix(GA): Fix ga initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
+import GA from 'react-ga'
+import { useMount } from 'react-use'
 import { ApolloProvider } from '@apollo/react-hooks'
 
 import { colors } from 'css/theme'
 import { GlobalStyles } from 'css/GlobalStyles'
 
-import { Home } from './pages/home'
-
 import 'leaflet/dist/leaflet.css'
+
+import { Home } from './pages/home'
 
 export const client = new ApolloClient({
   uri: process.env.API_URL
 })
 
-export const App = () => (
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Home />
-  </ApolloProvider>
-)
+const GA_KEY = process.env.GA_KEY || 'UA-000000-00'
+
+export const App = () => {
+  useMount(() => {
+    if (process.env.NODE_ENV === 'production') {
+      GA.initialize(GA_KEY)
+      GA.pageview(window.location.pathname)
+    }
+  })
+  return (
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Home />
+    </ApolloProvider>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { App } from './App'
-import ReactGA from 'react-ga'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize(process.env.GA_KEY || 'UA-000000-01')
-  ReactGA.pageview(window.location.pathname + window.location.search)
-}
