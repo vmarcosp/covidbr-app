9ea07bb779ac4705b3a721becc1800e4d6adf5d0
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-10-24 01:24:41 -0300

    feat: bar chart percentage

--- src/components/BarChart/components/BarChart/styles.tsx
+++ src/components/BarChart/components/BarChart/styles.tsx
@@ -1,17 +1,29 @@
 import styled from 'styled-components'
-import { BarChartData } from './index'
+import { fontFamily } from 'css/theme'
 
 const Container = styled.div`
+  width: 100%;
   display: flex;
 `
-
-const ChartItem = styled.div<BarChartData>`
-  width: ${props => props.totalValue}%;
-  min-width: 12px;
-  height: 12px;
-  background: ${props => props.color};
-  margin: 2px;
-  border-radius: 10px;
+const ChartBarContainer = styled.div<{totalValue: number}>`
+  width: ${({ totalValue }) => totalValue.toFixed(0)}%;
+  display: flex;
+  flex-direction: column;
+  margin-right: 4px;
 `
 
-export default { Container, ChartItem }
+const ChartItem = styled.div<{color: string}>`
+  height: 12px;
+  border-radius: 10px;
+  background: ${({ color }) => color};
+`
+
+const ChartPercentage = styled.span`
+  margin-bottom: 4px;
+  font-family: ${fontFamily};
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 12px;
+`
+
+export default { Container, ChartItem, ChartBarContainer, ChartPercentage }
